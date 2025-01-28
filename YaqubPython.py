a55e5c9a2673052084f64046e0c52f0082c7b034
Yaqub <yaqubmamme95@gmail.com>
2025-01-28 19:03:57 +0400

    hello world

---
+++ YaqubPython.py
@@ -0,0 +1,21 @@
+#print("Enter your number: ", end="")
+#number1 = int(input())
+#
+#
+#while number1>0:
+#    print("*")
+#    number1 = number1 - 1
+
+
+
+print("Enter first your number: ", end="")
+number1 = int(input())
+print("Enter your second number: ", end="")
+number2 = int(input())
+
+for i in range(0):
+    for j in range(0):
+        print("*", end="")
+    print("*")
+
+#2-ye ya ne ponyal
